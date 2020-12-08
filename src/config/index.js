ebcb4ea99a816b1fff63fd943b2641773908697f
Alexander <gituser1056@gmail.com>
2020-12-08 17:36:40 +0300

    Изменен URL API

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 // const URL_MAIN_API = 'http://localhost:3000';
-const URL_MAIN_API = 'https://api.diploma-2020.ru';
+// const URL_MAIN_API = 'https://api.diploma-2020.ru';
+const URL_MAIN_API = 'http://api.news-explore.ru';
 
 // const URL_NEWS_API = 'http://newsapi.org/v2/everything';
 const URL_NEWS_API = 'https://nomoreparties.co/news/v2/everything';
